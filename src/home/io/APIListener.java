d1948146a55ef316eac5c8fd958d8ae130d81da6
Haemrok <morlock@stud.fra-uas.de>
2020-01-27 12:26:58 +0100

    Ok....ich heiße nicht mehr "Maximus Morlockus"

--- src/home/io/APIListener.java
+++ src/home/io/APIListener.java
@@ -1,26 +1,21 @@
 package home.io;
-
 import home.model.Light;
-import home.model.Room;
 
 /**
  * <h1>APIListener</h1>
  * This interface should be implemented by classes that want to listen to the ingoing serial messages.
  *
- * @author Maximus Morlockus
- * @version 0.1
+ * @author Maximilian Morlock
+ * @version 1.0
  * @since 2019-11-30
  */
 public interface APIListener
 {
-  // og also hier kannst du alle INGOING callbacks definieren, die kann ich dann je nach implementierung des modells implementieren in einer
-  // klasse die das interface hier implementiert
-
+  //definition for ingoing callbacks
   void onLightSwitch(int roomID, int lightID, Light.State state);
   void onLightMode(int roomID, int lightID, Light.Mode mode);
   void onTemperature(int roomID, float actual);
   void onTemperatureReference(int roomID, float actual);
-  void onDebug(String message);
   void onStart_init();
   void onEnd_init();
 }
