d49eb23639b1fcb6e8da02bfad7a4c2f9bfd4176
Haemrok <morlock@stud.fra-uas.de>
2020-01-27 12:24:59 +0100

    code cleanup

--- src/home/io/CommunicationAPI.java
+++ src/home/io/CommunicationAPI.java
@@ -1,8 +1,6 @@
 package home.io;
 import home.Application;
 import home.model.Light;
-
-import java.time.chrono.IsoChronology;
 import java.util.Arrays;
 import java.util.logging.Level;
 
@@ -12,7 +10,7 @@ import java.util.logging.Level;
  * It uses an APIListener Interface set on creation to implement simple callbacks.
  *
  * @author Maximilian Morlock
- * @version -1000000
+ * @version 1.0
  * @since 2019-11-30
  */
 public class CommunicationAPI
@@ -30,14 +28,14 @@ public class CommunicationAPI
    */
   public static synchronized void initialize(APIListener listener)
   {
-
     CommunicationAPI.listener = listener;
-
   }
 
+  //Outgoing method for setLight
   public static synchronized void setLight(int roomid, int lightid, Light.State status){
     String s = (status == Light.State.LIGHT_ON) ? "on" : "off";
 
+    //Converting integer parameters to a single byte
     String hex1 = Integer.toHexString(roomid);
     byte rmID = Byte.parseByte(hex1, 16);
 
@@ -46,17 +44,24 @@ public class CommunicationAPI
 
     Byte[] message;
 
+    //Filling the array depending on the given light_state
     if (s.equals("off")){
       message = new Byte[]{0x02, rmID, lID, 0x00, 0x0d};
     }
     else {
       message = new Byte[]{0x02, rmID, lID, 0x01, 0x0d};
     }
-
+    //sending the message
     SerialIO.write(message);
   }
 
+
+
+
+
+  //Outgoing method for setLightMode
   public static synchronized void setLightMode(int roomid, int lightid, Light.Mode mode){
+    //same procedure as for lightState!
     String s = (mode == Light.Mode.MODE_MANUAL) ? "manual" : "auto";
 
     String hex1 = Integer.toHexString(roomid);
@@ -77,13 +82,23 @@ public class CommunicationAPI
     SerialIO.write(message);
   }
 
+
+
+  //Outgoing method for tempReference
   public static synchronized void tempReference(int roomid, float temp){
+    //To get the int value for everything before the decimal point
+    //the float value is simply parsed to int
     int vorkomma = (int) temp;
 
+    //Calculation for post-decimal places
+    //those need to be converted to an int value as well!
+    //Multiplication by 100 "deletes" the decimal point since only 2 places are significant.
+    //mod 100 calculation eliminates the pre-decimal places
     float nach = temp * 100;
     float nach2 = nach%100;
     int nachkomma = (int) nach2;
 
+    //converting everything to byte
     String hex1 = Integer.toHexString(roomid);
     Byte rmID = Byte.parseByte(hex1, 16);
 
@@ -94,7 +109,6 @@ public class CommunicationAPI
     Byte after = Byte.parseByte(hex3, 16);
 
     Byte[] message = new Byte[]{0x05, rmID, pre, after, 0x0d};
-    //System.out.println(Arrays.toString(message));
     SerialIO.write(message);
   }
 
@@ -115,11 +129,14 @@ public class CommunicationAPI
 
     if (listener == null ) {Application.debug("ERROR: LISTENER NOT AVAILABLE\r\n");}
 
+    //checking whether the incoming message has a valid length
     if (data.length != 4) {
       Application.debug("invalid length of received data", Level.WARNING);
     }
     else {
+      //The first bytes represents the operation that should be executed
       Byte instruction = data[0];
+
       switch (instruction) {
         //start_init
         case 0x00:
@@ -136,8 +153,11 @@ public class CommunicationAPI
           byte light_switch = data[3];
           Light.State state;
 
+          //depending on light_switch, the light state is set
+          //the other information are given and passed to the callback functions
           if (light_switch == 0x01){
             state = Light.State.LIGHT_ON;
+            //                     roomID   lightID  state
             listener.onLightSwitch(data[1], data[2], state);
           }
           else if (light_switch == 0x00){
@@ -148,11 +168,13 @@ public class CommunicationAPI
 
         //light_mode
         case 0x03:
+          //same as light_state!
           byte light_mode = data[3];
           Light.Mode mode;
 
           if (light_mode == 0x00){
             mode = Light.Mode.MODE_MANUAL;
+            //                   roomID   lightID  mode
             listener.onLightMode(data[1], data[2], mode);
           }
           else if (light_mode == 0x01){
@@ -163,24 +185,29 @@ public class CommunicationAPI
 
         //temperature
         case 0x04:
+            //pre- and post-decimal places
             float result = data[2];
             float decimal = data[3];
 
+            //bringing decimal places to float and add them with integer places
             decimal = decimal/100;
             result = result + decimal;
 
+            //                     roomID   temperature
             listener.onTemperature(data[1], result);
             Application.debug(result);
             break;
 
         //temperature_reference
         case 0x05:
+          //same as temperature
           float reference = data[2];
           float decimal2 = data[3];
 
           decimal = decimal2/100;
           result = reference + decimal2;
 
+          //                              roomID   temperature
           listener.onTemperatureReference(data[1], result);
           break;
 
